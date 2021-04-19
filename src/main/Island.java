6a363d0b7e492f00474593aff89c44b32c1ce27e
aerinn <han72@uclive.ac.nz>
2021-04-19 16:31:13 +1200

    update!!!

--- src/main/Island.java
+++ src/main/Island.java
@@ -1,9 +1,29 @@
 package main;
 
+import java.util.ArrayList;
+
 public class Island {
+
 	private String islandName;
+	private Store store;
+	private ArrayList<Route> routeList;
+	
+	public Island(String name, Store store, ArrayList<Route> routeList) {
+		this.islandName = name;
+		this.store = store;
+		this.routeList = routeList;
+	}
+	
+	public void addRoute() {
+		
+	}
+	
+	
 
 	public String getName() {
 		return islandName;
 	}
+	
+
+
 }
